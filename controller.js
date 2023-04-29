d4e5a08cd568fd0d3d08bc66705a2da23435750e
Nicolas <nicolas.aguilar180193@gmail.com>
2023-04-29 19:25:54 -0300

    Uso de ng-repeat con arreglo del

--- controller.js
+++ controller.js
@@ -1,4 +1,15 @@
 var app = angular.module("MyFirstApp", []);
 app.controller("FirstController", function($scope) {
     $scope.nombre = 'Nicolas';
+    $scope.comentarios = [
+        {
+            comentario: "Buen tutorial!",
+            username: "Nico322"
+        },
+        {
+            comentario: "Asco de tutorial >:(",
+            username: "hater123"
+        },
+        
+    ]
 })
